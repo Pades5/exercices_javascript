1594237788e9c9a0f657dc69de08072ed3f92626
Pades5 <you@example.com>
2024-12-05 21:19:08 +0100

    ajout du soir

---
+++ switch_to_if.js
@@ -0,0 +1,12 @@
+let browser = prompt("entrez votre navigateur svp");
+if (browser === "edge") {
+  alert("You've got the Edge!");
+} else if (browser === "chrome") {
+  alert("Okay we support these browsers too");
+} else if (browser === "safari") {
+  alert("Okay we support these browsers too");
+} else if (browser === "opera") {
+  alert("Okay we support these browsers too");
+} else if (browser === "firefox") {
+  alert("Okay we support these browsers too");
+} else alert("'We hope that this page looks ok!'");
